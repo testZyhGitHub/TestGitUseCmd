4426422875da9ce9a6f6c9232e470b56941bebcb
enju <zyhnanau86@163.com>
2018-08-25 19:15:51 +0800

    m

--- CompUseGitCmd.php
+++ CompUseGitCmd.php
@@ -5,4 +5,8 @@
 	
 	# 同时恢复多个被删除的文件：  必须是一次性删除的多个文件, 一个个删除了的话, 必须使用git checkout -- file方式一个个恢复!
 	git ls-files -d | xargs -i git checkout {}     
+	
+	// git pull时出现冲突 放弃本地修改，使远程库内容强制覆盖本地代码
+	git fetch --all //只是下载代码到本地，不进行合并操作
+	git reset --hard origin/master  //把HEAD指向最新下载的版本
 ?>
